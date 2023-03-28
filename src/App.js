433afdcbca75d103677a0dc331639cc0da3806c7
raymondkneipp <raymondkneipp@gmail.com>
2023-03-28 15:30:03 -0700

    add styles

--- src/App.js
+++ src/App.js
@@ -3,136 +3,167 @@ import './App.css'
 import butcherPigImage from './assets/butcherPig.jpeg'
 
 const App = () => {
+	// ACTION ITEM: to make the development process easier there are some preassigned words in the input field, when you are ready for your full user experience delete the test words passed to useState and pass an empty string
+	const [userInput, setUserInput] = useState('')
+	const [inputTranslated, setInputTranslated] = useState('')
 
-  // ACTION ITEM: to make the development process easier there are some preassigned words in the input field, when you are ready for your full user experience delete the test words passed to useState and pass an empty string
-  const [userInput, setUserInput] = useState("apple through queen squeal fry fluent")
-  const [inputTranslated, setInputTranslated] = useState("")
+	// ACTION ITEM: the "myPigLatinCodeHere" function is where you will put your logic to translate the sentence entered by the user into Pig Latin
+	const myPigLatinCodeHere = () => {
+		// NO MODIFICATION NEEDED: the variable "arrayOfUserInput" will contain the text input from the user split into an array of words
+		const arrayOfUserInput = userInput.split(' ')
+		console.log('arrayOfUserInput:', arrayOfUserInput)
 
-  // ACTION ITEM: the "myPigLatinCodeHere" function is where you will put your logic to translate the sentence entered by the user into Pig Latin
-  const myPigLatinCodeHere = () => {
+		// NO MODIFICATION NEEDED: now that we have an array of words, we can map over the array and look at each word
+		const translatedWordsArray = arrayOfUserInput.map((eachWord) => {
+			console.log('eachWord:', eachWord)
 
-    // NO MODIFICATION NEEDED: the variable "arrayOfUserInput" will contain the text input from the user split into an array of words
-    const arrayOfUserInput = userInput.split(" ")
-    console.log("arrayOfUserInput:", arrayOfUserInput)
+			// NO MODIFICATION NEEDED: this code will look at each word and identify the vowels
+			const vowelsArray = eachWord.split('').filter((vowel) => {
+				return (
+					vowel === 'a' ||
+					vowel === 'e' ||
+					vowel === 'i' ||
+					vowel === 'o' ||
+					vowel === 'u'
+				)
+			})
+			console.log('vowelsArray:', vowelsArray)
 
-    // NO MODIFICATION NEEDED: now that we have an array of words, we can map over the array and look at each word
-    const translatedWordsArray = arrayOfUserInput.map(eachWord => {
-      console.log("eachWord:", eachWord)
+			// ACTION ITEM: your Pig Latin logic goes here!
 
-      // NO MODIFICATION NEEDED: this code will look at each word and identify the vowels
-      const vowelsArray = eachWord.split("").filter(vowel => {
-        return (
-          vowel === "a" || 
-          vowel === "e" || 
-          vowel === "i" || 
-          vowel === "o" || 
-          vowel === "u"
-        )
-      })
-      console.log("vowelsArray:", vowelsArray)
+			// if word is capitalized then set capitalized to true
+			let capitalized = false
 
-      // ACTION ITEM: your Pig Latin logic goes here!
-     
-      const vowels = ['a', 'e', 'i', 'o', 'u']
-      
-      const punc = [".", "!", "?", ","]
+			if (eachWord.charAt(0) === eachWord.charAt(0).toUpperCase()) {
+				capitalized = true
+			}
 
-      const indexOfPunctuation = eachWord.split("").findIndex((value) => {
-        if (punc.includes(value)) {
-          return true
-        }
-      })
-    
-      const punctuation = eachWord.charAt(indexOfPunctuation)
-        eachWord = eachWord.replace(punctuation, "")
-        
-      if (vowels.includes(eachWord.charAt(0))) {
-        return eachWord.concat('way').concat(punctuation)
-      }
+			eachWord = eachWord.toLowerCase()
 
-      // qu words
-      if (eachWord.includes('qu')) {
-        let newWord = eachWord.split('qu')
-        return newWord[1].concat('qu').concat(newWord[0]).concat('ay').concat(punctuation)
-      }
+			const vowels = ['a', 'e', 'i', 'o', 'u']
 
-      // if word contains y and not vowel
-      if (eachWord.includes('y') && eachWord.split("").every(letter => !vowels.includes(letter))) {
-        let newWord = eachWord.split('y')
+			const punc = ['.', '!', '?', ',']
 
-        return 'y'.concat(newWord[0]).concat('ay').concat(punctuation)
-      }
+			const indexOfPunctuation = eachWord.split('').findIndex((value) => {
+				if (punc.includes(value)) {
+					return true
+				}
+			})
 
-      // words that have one or more consonats 
-     
-      const indexOfFirstVowel = eachWord.split("").findIndex((value) => {
-        if (vowels.includes(value)) {
-          return true
-        }
-      })
+			const punctuation = eachWord.charAt(indexOfPunctuation)
+			eachWord = eachWord.replace(punctuation, '')
 
-      let middleSection = eachWord.slice(indexOfFirstVowel)
-      let firstSection = eachWord.substring(0, indexOfFirstVowel)
+			if (vowels.includes(eachWord.charAt(0))) {
+				eachWord = eachWord.concat('way')
+			} else {
+				// qu words
+				if (eachWord.includes('qu')) {
+					let newWord = eachWord.split('qu')
+					eachWord = newWord[1].concat('qu').concat(newWord[0]).concat('ay')
+				} else {
+					// if word contains y and not vowel
+					if (
+						eachWord.includes('y') &&
+						eachWord.split('').every((letter) => !vowels.includes(letter))
+					) {
+						let newWord = eachWord.split('y')
 
-      return middleSection.concat(firstSection).concat("ay").concat(punctuation)
+						eachWord = 'y'.concat(newWord[0]).concat('ay')
+					} else {
+						// words that have one or more consonats
+						const indexOfFirstVowel = eachWord.split('').findIndex((value) => {
+							if (vowels.includes(value)) {
+								return true
+							}
+						})
 
-      // ACTION ITEM: this return will be the output of your Pig Latin'd code
-      return eachWord
-      
-    })
+						let middleSection = eachWord.slice(indexOfFirstVowel)
+						let firstSection = eachWord.substring(0, indexOfFirstVowel)
 
-    // NO MODIFICATION NEEDED: once the code has been modified it gets joined from an array back to a string
-    const translatedWords = translatedWordsArray.join(" ")
-    console.log("translatedWords:", translatedWords)
+						eachWord = middleSection.concat(firstSection).concat('ay')
+					}
+				}
+			}
 
-    // NO MODIFICATION NEEDED: this will update the inputTranslated variable in state
-    setInputTranslated(translatedWords)
-  }
+			// if word was capitalized then capitalize first letter
+			if (capitalized) {
+				eachWord = eachWord.charAt(0).toUpperCase().concat(eachWord.slice(1))
+			}
 
-  // ACTION ITEM: this method restarts the game by setting the original state, when you are ready for your full user experience delete the test words in setUserInput and pass an empty string
-  const restartGame = () => {
-    setUserInput("apple through queen squeal fry fluent")
-    setInputTranslated("")
-  }
+			// add punctuation to end of word
+			eachWord = eachWord.concat(punctuation)
 
-  // NO MODIFICATION NEEDED: this method prevents React from refreshing the page unnecessarily
-  const setUpPreventDefault = (e) => {
-    e.preventDefault()
-    myPigLatinCodeHere()
-  }
+			// ACTION ITEM: this return will be the output of your Pig Latin'd code
+			return eachWord
+		})
 
-  // NO MODIFICATION NEEDED: this method takes the value of the input and saves it in state
-  const handleInput = (e) => {
-    setUserInput(e.target.value)
-  }
+		// NO MODIFICATION NEEDED: once the code has been modified it gets joined from an array back to a string
+		const translatedWords = translatedWordsArray.join(' ')
+		console.log('translatedWords:', translatedWords)
 
-  return (
-    <div className="page-container">
-      <div className="body-container">
-        <h1>Pig Latin Translator</h1>
-        <img
-          src={butcherPigImage}
-          alt="pig with butcher cut names in pig latin"
-          className="butcher-pig-image"
-        />
+		// if user inputs any numbers display 'unable to translate'
+		const containsNum = translatedWordsArray.some((word) => {
+			return word.split('').some((letter) => {
+				if (!isNaN(letter)) {
+					return true
+				}
+			})
+		})
 
-        <div className="input-section">
-          <h4>Enter phrase to be translated:</h4>
-          <input
-            type="text"
-            className="user-input"
-            onChange={handleInput}
-            value={userInput}
-          />
-          <br />
-          <button onClick={setUpPreventDefault}>Submit</button>
-          <button onClick={restartGame}>Clear</button>
-        </div>
-        <p>{inputTranslated}</p>
-      </div>
-      <footer>&copy; 2022 | Coded by: Your Names Here!</footer>
-    </div>
-  )
+		if (containsNum) {
+			setInputTranslated('Can not process numbers')
+		} else {
+			// NO MODIFICATION NEEDED: this will update the inputTranslated variable in state
+			setInputTranslated(translatedWords)
+		}
+	}
+
+	// ACTION ITEM: this method restarts the game by setting the original state, when you are ready for your full user experience delete the test words in setUserInput and pass an empty string
+	const restartGame = () => {
+		setUserInput('')
+		setInputTranslated('')
+	}
+
+	// NO MODIFICATION NEEDED: this method prevents React from refreshing the page unnecessarily
+	const setUpPreventDefault = (e) => {
+		e.preventDefault()
+		myPigLatinCodeHere()
+	}
+
+	// NO MODIFICATION NEEDED: this method takes the value of the input and saves it in state
+	const handleInput = (e) => {
+		setUserInput(e.target.value)
+	}
+
+	return (
+		<div className="page-container">
+			<div className="body-container">
+				<h1>Pig Latin Translator</h1>
+				<img
+					src={butcherPigImage}
+					alt="pig with butcher cut names in pig latin"
+					className="butcher-pig-image"
+				/>
+
+				<div className="input-section">
+					<h4>Enter phrase to be translated:</h4>
+					<input
+						type="text"
+						className="user-input"
+						onChange={handleInput}
+						value={userInput}
+					/>
+					<br />
+					<button onClick={setUpPreventDefault} className="primary">
+						Submit
+					</button>
+					<button onClick={restartGame}>Clear</button>
+				</div>
+				<p>{inputTranslated}</p>
+			</div>
+			<footer>&copy; 2022 | Coded by: Jose & Raymond</footer>
+		</div>
+	)
 }
 
 export default App
